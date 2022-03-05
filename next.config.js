3a1e4d58ac39170bb90c86adce1a490216f6c70f
hisho <hisho.web@gmail.com>
2022-03-05 17:16:34 +0900

    ✨ i18n

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts'],
+  /**
+   * @see https://nextjs.org/docs/advanced-features/i18n-routing
+   * @see https://zenn.dev/steelydylan/articles/nextjs-with-i18n
+   */
+  i18n: {
+    locales: ['en', 'ja'],
+    defaultLocale: 'ja',
+  },
 }
 
 module.exports = nextConfig
