c856130fba18e090234a6165865eb808c5493cec
hisho <hisho.web@gmail.com>
2022-03-06 01:31:45 +0900

    ✨ update

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -5,31 +5,52 @@ import { Input } from '@src/components/ui/Input/Input'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
+import NextLink from 'next/link'
+import { useLocale } from '@src/hooks/useLocale'
+import { useState } from 'react'
 
 const schema = z.object({
   name: z.string(),
   email: z.string().email(),
 })
 
+const sleep = (ms: number = 1000) =>
+  new Promise<void>((resolve) => {
+    setTimeout(resolve, ms)
+  })
+
 const Home: NextPage = () => {
   const { control, handleSubmit, formState } = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
   })
+  const { isJapanese } = useLocale()
+  const [isLoading, setLoading] = useState(false)
 
   const onSubmit = handleSubmit(async ({ name, email }) => {
+    setLoading(true)
     try {
-      console.log({ name })
-      console.log({ email })
+      await sleep()
+      console.table({ name, email })
     } catch (e) {
       if (e instanceof Error) {
         console.log(e.message)
       }
+    } finally {
+      setLoading(false)
     }
   })
 
   return (
     <Container>
       <Heading textAlign={'center'}>next js with zod</Heading>
+      <Grid mt={5} gap={2} gridTemplateColumns={'repeat(2,1fr)'}>
+        <NextLink href={'/'} locale={'ja'} passHref>
+          <Button as={'a'}>ja</Button>
+        </NextLink>
+        <NextLink href={'/'} locale={'en'} passHref>
+          <Button as={'a'}>en</Button>
+        </NextLink>
+      </Grid>
       <chakra.form
         mt={12}
         px={4}
@@ -44,14 +65,18 @@ const Home: NextPage = () => {
       >
         <Grid gap={6}>
           <FormControl
-            formLabel={'名前'}
+            formLabel={isJapanese ? '名前' : 'name'}
             // isRequired
             errorMessage={formState.errors.name?.message}
           >
-            <Input control={control} name={'name'} placeholder={'名前 太郎'} />
+            <Input
+              control={control}
+              name={'name'}
+              placeholder={isJapanese ? '名前 太郎' : 'Name Taro'}
+            />
           </FormControl>
           <FormControl
-            formLabel={'メールアドレス'}
+            formLabel={isJapanese ? 'メールアドレス' : 'email'}
             // isRequired
             errorMessage={formState.errors.email?.message}
           >
@@ -63,8 +88,8 @@ const Home: NextPage = () => {
             />
           </FormControl>
         </Grid>
-        <Button mt={6} type={'submit'} mx={'auto'}>
-          アカウントの作成
+        <Button mt={6} type={'submit'} mx={'auto'} isLoading={isLoading}>
+          {isJapanese ? 'アカウントの作成' : 'create account'}
         </Button>
       </chakra.form>
     </Container>
