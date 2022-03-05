3e9871f4514530f18c9b4696091dcd3e9160ea0a
hisho <hisho.web@gmail.com>
2022-03-05 17:27:28 +0900

    ✨ create useLocale

---
+++ src/hooks/useLocale.ts
@@ -0,0 +1,22 @@
+import { NextRouter, useRouter } from 'next/router'
+
+const localeMap = (locale: NextRouter['locale']) => {
+  switch (locale) {
+    case 'ja':
+      return 'ja'
+    case 'en':
+      return 'en'
+    default:
+      return 'ja'
+  }
+}
+
+/**
+ * @see https://zenn.dev/steelydylan/articles/nextjs-with-i18n
+ */
+export const useLocale = (): { locale: ReturnType<typeof localeMap> } => {
+  const { locale: _locale } = useRouter()
+
+  const locale = localeMap(_locale)
+  return { locale }
+}
