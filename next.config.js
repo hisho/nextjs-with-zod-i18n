1ca863a6ed042f9e2c15b95eac9054cb3ae27a09
hisho <hisho.web@gmail.com>
2022-03-05 17:17:05 +0900

    📝 add comment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * @see https://nextjs.org/docs/api-reference/next.config.js/custom-page-extensions
+   */
   pageExtensions: ['page.tsx', 'page.ts'],
   /**
    * @see https://nextjs.org/docs/advanced-features/i18n-routing
