67fa4381f47cbc92d7e2869cdf90d57c910f2011
hisho <hisho.web@gmail.com>
2022-03-05 16:07:53 +0900

    🎉 init

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import { Container, Heading } from '@chakra-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <Container>
+      <Heading textAlign={'center'}>next js with zod</Heading>
+    </Container>
+  )
+}
+
+export default Home
