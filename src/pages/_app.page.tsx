dd00ecc7bca54ec689a92d8d80f6969ca4120f19
hisho <hisho.web@gmail.com>
2022-05-01 23:10:45 +0900

    ✨ update

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,13 +1,20 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-import { ZodErrorMap } from '@src/libs/validation/ZodErrorMap'
+import { useLocale } from '@src/hooks/useLocale'
+import { zodCustomErrorMap } from '@src/libs/validation/zodCustomErrorMap'
+import { z } from 'zod'
 
+/**
+ * @see https://github.com/colinhacks/zod/blob/master/ERROR_HANDLING.md#global-error-map
+ * @see https://github.com/colinhacks/zod/blob/cbbfedd15ffbe7d880f52d6becb76dcaef54875f/src/ZodError.ts#L284
+ */
 function MyApp({ Component, pageProps }: AppProps) {
+  const { locale } = useLocale()
+  z.setErrorMap(zodCustomErrorMap(locale))
+
   return (
     <ChakraProvider>
-      <ZodErrorMap>
-        <Component {...pageProps} />
-      </ZodErrorMap>
+      <Component {...pageProps} />
     </ChakraProvider>
   )
 }
